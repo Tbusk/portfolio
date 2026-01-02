6365907ae6d3485a617d26c79b3e51a3d2a3bd78
Trevor Busk <trevor@tbusk.com>
2026-01-02 18:50:01 -0500

    feat: create open source contribution component

---
+++ src/components/OpenSourceContribution.tsx
@@ -0,0 +1,28 @@
+interface OpenSourceContributionProps {
+    name: string;
+    link: string;
+    content: string[];
+}
+
+export default function OpenSourceContribution(props: OpenSourceContributionProps) {
+    return (
+        <>
+            <div>
+                <div className="text-2xl pb-2 text-center md:text-start px-2">
+                    <a href={props.link}>
+                        {props.name}
+                    </a>
+                </div>
+                <div>
+                    <ul className="list-disc pl-6 md:pl-4">
+                        {props.content.map((content) => (
+                            <li className="pl-2 py-1.5">
+                                {content}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
