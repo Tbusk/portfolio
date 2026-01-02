c8484bdfaea4d0cd6395353ff7dbc18e8073a042
Trevor Busk <trevor@tbusk.com>
2026-01-02 18:55:18 -0500

    feat: add and populate socials component

---
+++ src/components/Socials.tsx
@@ -0,0 +1,24 @@
+import githubIcon from '../assets/github_icon.svg';
+import linkedinIcon from '../assets/linkedin_icon.svg';
+import Social from "./Social.tsx";
+
+export default function Socials() {
+    return (
+        <>
+            <div className="flex flex-col md:flex-row justify-center gap-3 py-8">
+                <Social
+                    name="Github"
+                    link="https://github.com/Tbusk"
+                    logo={githubIcon}
+                    logoAlt="GitHub Icon"
+                />
+                <Social
+                    name="LinkedIn"
+                    link="https://www.linkedin.com/in/trevor-busk/"
+                    logo={linkedinIcon}
+                    logoAlt="LinkedIn Icon"
+                />
+            </div>
+        </>
+    );
+}
