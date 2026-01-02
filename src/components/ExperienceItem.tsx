511e0c668fce7b3cfefbdaa1793ce707356b8696
Trevor Busk <trevor@tbusk.com>
2026-01-02 18:50:20 -0500

    feat: create experience item component

---
+++ src/components/ExperienceItem.tsx
@@ -0,0 +1,38 @@
+interface ExperienceItemProps {
+    company: string;
+    website?: string;
+    jobTitle: string;
+    description: string[];
+    startDate: string;
+    endDate: string;
+}
+
+export default function ExperienceItem(props: ExperienceItemProps) {
+    return (
+        <>
+            <div>
+
+                <div className="text-lg text-center md:text-start pb-2">
+                    {props.startDate} - {props.endDate}
+                </div>
+
+                <div className="text-2xl text-center md:text-start font-bold pb-2">
+                    {props.jobTitle}
+                </div>
+
+                <div className="text-lg text-center md:text-start pb-2">
+                    {props.website ? <a href={props.website}>{props.company}</a> : `${props.company}`}
+                </div>
+
+                <ul className="list-disc pl-4">
+                    {props.description.map((content) => (
+                        <li className="pl-2 py-1.5">
+                            {content}
+                        </li>
+                    ))}
+                </ul>
+
+            </div>
+        </>
+    );
+}
