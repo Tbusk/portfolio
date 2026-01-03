a5802ea75d95a33cd688825103f356c86c87716e
Trevor Busk <trevor@tbusk.com>
2026-01-03 18:12:53 -0500

    change: reduce title sizes a little

--- src/components/open_source/OpenSourceContributions.tsx
+++ src/components/open_source/OpenSourceContributions.tsx
@@ -5,7 +5,7 @@ export default function OpenSourceContributions() {
         <>
             <div className="flex flex-col pb-4">
                 <div>
-                    <div className="text-3xl md:text-6xl px-2 md:px-0 py-1 md:py-4" id="opensource">
+                    <div className="text-3xl md:text-5xl px-2 md:px-0 py-1 md:py-4" id="opensource">
                         Open Source Contributions
                     </div>
                 </div>
