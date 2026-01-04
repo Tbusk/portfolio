6485cd95d2b3679c00850c10f06e8e340f21a446
Trevor Busk <trevor@tbusk.com>
2026-01-04 16:14:28 -0500

    feat: add coursework to education component + misc style tweaks

--- src/components/education/EducationItem.tsx
+++ src/components/education/EducationItem.tsx
@@ -1,3 +1,5 @@
+import Pills from "../common/pills/Pills.tsx";
+
 interface EducationItemProps {
     name: string;
     institution: string;
@@ -5,6 +7,7 @@ interface EducationItemProps {
     startDate: string;
     endDate: string;
     gpa?: string;
+    coursework?: string[];
 }
 
 export default function EducationItem(props: EducationItemProps) {
@@ -14,17 +17,26 @@ export default function EducationItem(props: EducationItemProps) {
                 <div className="text-lg pb-2 px-2">
                     {props.startDate} - {props.endDate}
                 </div>
+
                 <div className="text-2xl font-bold pb-2  px-2">
                     {props.name}
                 </div>
-                <div className="text-lg pb-2  px-2">
+
+                <div className="text-lg pb-2 px-2">
                     <a href={props.link} className="hover:underline">
-                        {props.institution}
+                        {props.gpa ? `${props.institution}, GPA: ${props.gpa}` : props.institution}
                     </a>
                 </div>
-                <div className="text-md  px-2">
-                    {props.gpa ? `GPA: ${props.gpa}` : ''}
-                </div>
+
+                {props.coursework && props.coursework.length > 0 && (
+                    <>
+                        <div className="font-bold px-2 py-2 pb-4">
+                            Relevant Coursework
+                        </div>
+
+                        <Pills items={props.coursework} />
+                    </>
+                )}
             </div>
         </>
     );
