bbb507893778028728c9a4241e7981ad2ebfb331
Trevor Busk <trevor@tbusk.com>
2026-01-04 16:18:03 -0500

    feat: impl SkillItems, SectionTitle comp in Skills comp

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -1,93 +1,87 @@
-import Skill from "./Skill.tsx";
+
+import SkillItems from "./SkillItems.tsx";
+import SectionTitle from "../common/SectionTitle.tsx";
 
 export default function Skills() {
     return (
         <>
             <div className="pb-8">
-                <div className="text-3xl md:text-5xl py-1 md:py-4 px-2 md:px-0" id="skills">
-                    Skills
-                </div>
 
-                <div className="text-xl px-2 md:px-0 font-bold">
-                    Frameworks
-                </div>
+                <SectionTitle title="Skills" id="skills"/>
 
-                <div className="flex flex-wrap gap-2 py-4 px-2 md:px-0">
-                    <Skill name="React" />
-                    <Skill name="Preact" />
-                    <Skill name="Spring" />
-                    <Skill name="Spring Boot" />
-                    <Skill name="Hibernate" />
-                    <Skill name="Apache Freemarker" />
-                    <Skill name="Tailwind CSS" />
-                    <Skill name="Bootstrap" />
-                    <Skill name="VitePress" />
-                    <Skill name="PayloadCMS" />
-                </div>
+                <SkillItems
+                    title="Frameworks"
+                    items={[
+                        "React",
+                        "Preact",
+                        "Spring",
+                        "Spring Boot",
+                        "Hibernate",
+                        "Apache Freemarker",
+                        "Tailwind CSS",
+                        "Bootstrap",
+                        "VitePress",
+                        "PayloadCMS",
+                        "Thymeleaf",
+                    ]}
+                />
 
-                <div className="text-xl px-2 md:px-0 font-bold">
-                    Languages
-                </div>
+                <SkillItems
+                    title="Languages"
+                    items={[
+                        "Java",
+                        "TypeScript",
+                        "JavaScript",
+                        "Python",
+                        "Vala",
+                        "HTML",
+                        "CSS",
+                        "Markdown"
+                    ]}
+                />
 
-                <div className="flex flex-wrap gap-2 py-4 px-2 md:px-0">
-                    <Skill name="Java" />
-                    <Skill name="TypeScript" />
-                    <Skill name="JavaScript" />
-                    <Skill name="Python" />
-                    <Skill name="Vala" />
-                    <Skill name="HTML" />
-                    <Skill name="CSS" />
-                    <Skill name="Markdown" />
-                </div>
+                <SkillItems
+                    title="Databases"
+                    items={[
+                        "MariaDB",
+                        "MS SQL Server",
+                        "PostgreSQL",
+                    ]}
+                />
 
-                <div className="text-xl px-2 md:px-0 font-bold">
-                    Databases
-                </div>
+                <SkillItems
+                    title="Tooling"
+                    items={[
+                        "GitHub",
+                        "GitHub Actions",
+                        "GitHub Projects",
+                        "Docker",
+                        "Docker Compose",
+                        "Dockerfile",
+                        "Git",
+                        "GitLab",
+                        "BitBucket",
+                        "Confluence",
+                        "Figma"
+                    ]}
+                />
 
-                <div className="flex flex-wrap gap-2 py-4 px-2 md:px-0">
-                    <Skill name="MariaDB" />
-                    <Skill name="MS SQL Server" />
-                    <Skill name="PostgreSQL" />
-                </div>
+                <SkillItems
+                    title="Deployments"
+                    items={[
+                        "Hetzner",
+                        "On Site",
+                        "Google Cloud",
+                        "AWS",
+                        "GitHub Pages"
+                    ]} />
 
-                <div className="text-xl px-2 md:px-0 font-bold">
-                    Tooling
-                </div>
-
-                <div className="flex flex-wrap gap-2 py-4 px-2 md:px-0">
-                    <Skill name="GitHub" />
-                    <Skill name="GitHub Actions" />
-                    <Skill name="GitHub Projects" />
-                    <Skill name="Docker" />
-                    <Skill name="Docker Compose" />
-                    <Skill name="Dockerfile" />
-                    <Skill name="Git" />
-                    <Skill name="GitLab" />
-                    <Skill name="BitBucket" />
-                    <Skill name="Confluence" />
-                   
-                </div>
-
-                <div className="text-xl px-2 md:px-0 font-bold">
-                    Deployments
-                </div>
-
-                <div className="flex flex-wrap gap-2 py-4 px-2 md:px-0">
-                    <Skill name="Hetzner" />
-                    <Skill name="On Site" />
-                    <Skill name="Google Cloud" />
-                    <Skill name="AWS" />
-                    <Skill name="GitHub Pages" />
-                </div>
-
-                <div className="text-xl px-2 md:px-0 font-bold">
-                    Environments
-                </div>
-
-                <div className="flex flex-wrap gap-2 pt-4 px-2 md:px-0">
-                    <Skill name="Linux" />
-                    <Skill name="Windows" />
-                </div>
+                <SkillItems
+                    title="Development / Deployment Environments"
+                    items={[
+                        "Linux",
+                        "Windows"
+                    ]} />
             </div>
         </>
     );
