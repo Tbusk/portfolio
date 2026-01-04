8aa66920f294129c881a5902f6a9f6d51f73826e
Trevor Busk <trevor@tbusk.com>
2026-01-04 16:24:16 -0500

    feat: add keys to reference via index

--- src/components/header/HeaderListItem.tsx
+++ src/components/header/HeaderListItem.tsx
@@ -1,11 +1,12 @@
 interface HeaderListItemProps {
     name: string;
     id: string;
+    index: number;
 }
 
 export default function HeaderListItem(props: HeaderListItemProps) {
     return (
-        <li className="hover:underline hover:scale-110 text-xl hidden sm:block">
+        <li className="hover:underline hover:scale-110 text-xl hidden sm:block" key={props.index}>
             <a href={`#${props.id}`}>
                 {props.name}
             </a>
--- src/components/header/NavbarContent.tsx
+++ src/components/header/NavbarContent.tsx
@@ -4,12 +4,12 @@ export default function NavbarContent() {
     return (
         <>
             <ul className="gap-8 justify-center hidden md:flex py-8">
-                <HeaderListItem name="Home" id="home" />
-                <HeaderListItem name="Experience" id="experience" />
-                <HeaderListItem name="Skills" id="skills" />
-                <HeaderListItem name="Education" id="education" />
-                <HeaderListItem name="Projects" id="projects" />
-                <HeaderListItem name="Open Source" id="opensource" />
+                <HeaderListItem name="Home" id="home" index={0}/>
+                <HeaderListItem name="Experience" id="experience" index={1} />
+                <HeaderListItem name="Skills" id="skills" index={2} />
+                <HeaderListItem name="Education" id="education" index={3} />
+                <HeaderListItem name="Projects" id="projects" index={4} />
+                <HeaderListItem name="Open Source" id="opensource" index={5} />
             </ul>
         </>
     );
