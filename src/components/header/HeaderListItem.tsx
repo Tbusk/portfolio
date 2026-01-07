faa923a538348ba9fcc34e0481922b6bf2488eda
Trevor Busk <trevor@tbusk.com>
2026-01-07 13:58:45 -0500

    feat: add underlining to active nav item

--- src/components/header/HeaderListItem.tsx
+++ src/components/header/HeaderListItem.tsx
@@ -5,8 +5,11 @@ interface HeaderListItemProps {
 }
 
 export default function HeaderListItem(props: HeaderListItemProps) {
+
+    const highlight = document.documentURI.endsWith(`/${props.id ? props.id : ''}`)
+
     return (
-        <li className="hover:underline hover:scale-110 text-xl hidden sm:block" key={props.index}>
+        <li className={`hover:underline hidden sm:block pt-7 text-xl ${highlight ? 'pb-6 border-b-3 border-gray-400 font-bold' : ''}`} key={props.index}>
             <a href={`/${props.id ? props.id : ''}`}>
                 {props.name}
             </a>
--- src/components/header/HeaderListItemMobile.tsx
+++ src/components/header/HeaderListItemMobile.tsx
@@ -10,8 +10,11 @@ interface HeaderListItemMobileProps {
 }
 
 export default function HeaderListItemMobile(props: HeaderListItemMobileProps) {
+
+    const highlight = document.documentURI.endsWith(`/${props.id ? props.id : ''}`)
+
     return (
-        <li className="hover:underline hover:scale-110 text-xl py-3 text-center" key={props.index}>
+        <li className={`hover:underline hover:scale-110 text-xl py-3 text-center ${highlight ? 'underline' : ''}`} key={props.index}>
             <a href={`/${props.id ? props.id : ''}`} onClick={() => props.setIsOpen(!props.isOpen)}>
                 {props.name}
             </a>
