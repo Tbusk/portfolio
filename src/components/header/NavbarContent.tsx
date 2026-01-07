be3df62e6ca3f82d71b59ffb6326fe601d3158d5
Trevor Busk <trevor@tbusk.com>
2026-01-07 13:16:21 -0500

    change: open-source id and remove id from homepage component

--- src/components/header/NavbarContent.tsx
+++ src/components/header/NavbarContent.tsx
@@ -4,12 +4,12 @@ export default function NavbarContent() {
     return (
         <>
             <ul className="gap-8 justify-center hidden md:flex py-8">
-                <HeaderListItem name="Home" id="home" index={0}/>
+                <HeaderListItem name="Home" index={0}/>
                 <HeaderListItem name="Experience" id="experience" index={1} />
                 <HeaderListItem name="Skills" id="skills" index={2} />
                 <HeaderListItem name="Education" id="education" index={3} />
                 <HeaderListItem name="Projects" id="projects" index={4} />
-                <HeaderListItem name="Open Source" id="opensource" index={5} />
+                <HeaderListItem name="Open Source" id="open-source" index={5} />
             </ul>
         </>
     );
