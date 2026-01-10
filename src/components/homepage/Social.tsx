c58e1fb51acc57374d4c7c14163e64eee27a6844
Trevor Busk <trevor@tbusk.com>
2026-01-10 17:34:45 -0500

    change: change styling of social component

--- src/components/homepage/Social.tsx
+++ src/components/homepage/Social.tsx
@@ -10,9 +10,10 @@ interface SocialProps {
 export default function Social(props: SocialProps) {
     return (
         <>
-            <div className="flex justify-center items-center rounded-full p-4 text-xl md:text-2xl hover:scale-105 border-2 border-[#5294e2]">
-                <a href={props.link}>
+            <div className="flex justify-center items-center rounded-2xl px-8 py-2 hover:scale-105 border-2 border-[#5294e2] hover:shadow-md">
+                <a href={props.link} className="flex items-center gap-2">
                     <img src={props.logo} alt={props.logoAlt} height={props.imageHeight} width={props.imageWidth}/>
+                    <span className="text-xl md:text-2xl">{props.name}</span>
                 </a>
             </div>
         </>
