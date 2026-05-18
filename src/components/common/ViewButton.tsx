2b69ed5d401fbe30c4c08a16d203a40bc14d8a4b
Trevor Busk <trevor@tbusk.com>
2026-05-18 18:41:22 -0400

    change: how buttons look on different modes

--- src/components/common/ViewButton.tsx
+++ src/components/common/ViewButton.tsx
@@ -5,8 +5,8 @@ interface ViewButtonProps {
 export default function ViewButton(props: ViewButtonProps) {
     return (
         <div className="pt-4 flex">
-            <a href={props.link} className="rounded-3xl  py-1.5 px-10 dark:border-(--darkmode-border-color) bg-(--accent-color) hover:bg-(--accent-color-hover)">
-                <div className="text-lg font-bold text-(--darkmode-text-color-primary)">
+            <a href={props.link} className="rounded-3xl  py-1.5 px-10 text-(--accent-color) bg-(--lightmode-background-color) dark:bg-(--darkmode-background-color) border-2 border-(--accent-color) dark:hover:bg-(--darkmode-background-color-hover) hover:scale-105">
+                <div className="text-lg dark:text-(--darkmode-text-color-primary) font-bold dark:font-normal">
                     View
                 </div>
             </a>
--- src/components/homepage/Social.tsx
+++ src/components/homepage/Social.tsx
@@ -10,7 +10,7 @@ export interface SocialProps {
 
 export default function Social(props: SocialProps) {
     return (
-        <div className="flex justify-center items-center rounded-2xl px-8 py-2 hover:scale-105 border-2 border-(--accent-color) hover:shadow-md">
+        <div className="flex justify-center items-center rounded-4xl px-8 py-2 hover:scale-105 border-2 border-(--accent-color) hover:shadow-md dark:hover:bg-(--darkmode-background-color-hover)">
             <a href={props.link} className="flex items-center gap-2">
                 <img src={props.logo} alt={props.logoAlt} height={props.imageHeight} width={props.imageWidth}/>
                 <span className="text-xl md:text-2xl dark:text-(--darkmode-text-color-secondary)">{props.name}</span>
