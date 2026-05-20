1f139b155c2a67b76d3ac8ac35b0fd073240c5de
Trevor Busk <trevor@tbusk.com>
2026-05-20 19:48:35 -0400

    patch: fix h-scroll issue on a few pages

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,8 +4,8 @@ import NavbarContent from "./NavbarContent.tsx";
 export default function Header() {
 
     return (
-        <header className="sticky top-0 bg-(--lightmode-background-color) dark:bg-(--darkmode-background-color) w-screen z-50">
-            <nav className="absolute w-screen bg-(--lightmode-background-color) dark:bg-(--darkmode-background-color) border-b-2 border-(--lightmode-border-color) dark:border-(--darkmode-border-color)">
+        <header className="sticky top-0 bg-(--lightmode-background-color) dark:bg-(--darkmode-background-color) w-full z-50">
+            <nav className="absolute w-full bg-(--lightmode-background-color) dark:bg-(--darkmode-background-color) border-b-2 border-(--lightmode-border-color) dark:border-(--darkmode-border-color)">
                 <NavbarContent/>
                 <NavbarContentMobile/>
             </nav>
