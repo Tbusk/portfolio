cbd0e1184bc2633ae1b8c2b2f65d118efd2c8f71
Trevor Busk <trevor@tbusk.com>
2026-07-31 18:20:53 -0400

    change: remove hardcoding of name in header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,17 @@
 import NavbarContentMobile from "./NavbarContentMobile.tsx";
 import NavbarContent from "./NavbarContent.tsx";
 import HeaderNameItem from "./HeaderNameItem.tsx";
+import myInfoJson from "../../data/my-info.json";
 
 export default function Header() {
+
+    const myInfo = myInfoJson;
 
     return (
         <header className="sticky top-0 bg-(--lightmode-background-color) dark:bg-(--darkmode-background-color) z-50">
             <nav
                 className="hidden md:flex md:pb-4 md:pt-2 md:justify-between md:items-end bg-(--lightmode-background-color) dark:bg-(--darkmode-background-color) border-b-2 border-(--lightmode-border-color) dark:border-(--darkmode-border-color)">
-                <HeaderNameItem name="Trevor Busk"/>
+                <HeaderNameItem name={`${myInfo.firstName} ${myInfo.lastName}`} />
                 <NavbarContent/>
             </nav>
 
